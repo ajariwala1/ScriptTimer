bcce5d4b8e2ead0e30e3b1872e22f3c9c2c21fe9
Abhishek Jariwala <ajariwala@auburn.edu>
2022-12-08 08:21:36 -0600

    Initial Commit

---
+++ ScriptTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScriptTimer
+//
+//  Created by Abhishek Jariwala on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
